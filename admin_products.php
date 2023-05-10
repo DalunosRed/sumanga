21d158940c1b145d48138d9c5333be4300abf882
Red L. Dalunos <78433881+DalunosRed@users.noreply.github.com>
2023-05-10 15:30:43 +0800

    Update admin_products.php

--- admin_products.php
+++ admin_products.php
@@ -14,31 +14,37 @@ if(isset($_POST['add_product'])){
 
    $name = mysqli_real_escape_string($conn, $_POST['name']);
    $price = $_POST['price'];
-   $image = $_FILES['image']['name'];
+   $image = $name.'_img1.' . pathinfo($_FILES['image']['name'],PATHINFO_EXTENSION);
+   $image = filter_var($image, FILTER_SANITIZE_STRING);
    $image_size = $_FILES['image']['size'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
    $image_folder = 'uploaded_img/'.$image;
    $category = $_POST['category'];
 
+   $allowed = array('png', 'jpg', 'jpeg', 'webp', 'JPG');
+   $ext = pathinfo($image, PATHINFO_EXTENSION);
+
    $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name'") or die('query failed');
 
-   if(mysqli_num_rows($select_product_name) > 0){
-      $warning_msg[] = 'product name already added';
-   }else{
-      $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, image, category) VALUES('$name','$price', '$image', '$category')") or die('query failed');
-
-      if($add_product_query){
-         if($image_size > 2000000){
-            $message[] = 'image size is too large';
-         }else{
-            move_uploaded_file($image_tmp_name, $image_folder);
-            $success_msg[] = 'product added successfully!';
-         }
+   if($image != $name.'_img1.'){
+      if (!in_array($ext, $allowed)) {
+         $warning_msg[] = 'Only png, jpg, jpeg and webp are allowed!';
       }else{
-         $warning_msg[] = 'product could not be added!';
-      }
+         if(mysqli_num_rows($select_product_name) > 0){
+         $warning_msg[] = 'product name already added';
+      }else{
+            if( $_FILES['image']['size'] > 2000000){
+               $error_msg[] = 'image size is too large';
+            }else{
+               $add_product_query = mysqli_query($conn, "INSERT INTO `products`(name, price, image, category) VALUES('$name','$price', '$image', '$category')") or die('query failed');
+               move_uploaded_file($image_tmp_name, $image_folder);
+               $success_msg[] = 'product added successfully!';
+            }}
+
+         }
    }
 }
+
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
@@ -53,11 +59,13 @@ if(isset($_POST['update_product'])){
 
    $update_p_id = $_POST['update_p_id'];
    $update_name = $_POST['update_name'];
+   $update_cat = $_POST['update_cat'];
    $update_price = $_POST['update_price'];
 
-   mysqli_query($conn, "UPDATE `products` SET name = '$update_name', price = '$update_price' WHERE id = '$update_p_id'") or die('query failed');
+   mysqli_query($conn, "UPDATE `products` SET category ='$update_cat', name ='$update_name', price = '$update_price' WHERE id = '$update_p_id'") or die('query failed');
 
    $update_image = $_FILES['update_image']['name'];
+   $update_image = $_FILES['update_image']['cat'];
    $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
    $update_image_size = $_FILES['update_image']['size'];
    $update_folder = 'uploaded_img/'.$update_image;
@@ -73,7 +81,9 @@ if(isset($_POST['update_product'])){
       }
    }
 
-   header('location:admin_products.php');
+   
+   $success_msg[] = 'payment status has been updated!';
+
 
 }
 
@@ -90,6 +100,12 @@ if(isset($_POST['update_product'])){
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
    <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
@@ -101,17 +117,39 @@ if(isset($_POST['update_product'])){
 <!-- product CRUD section starts  -->
 
 <section class="add-products">
-
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Add product
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
    <h1 class="title">shop products</h1>
 
-   <form action="" method="post" enctype="multipart/form-data">
-      <h3>add product</h3>
-      <input type="text" name="name" class="box" placeholder="enter product name" required>
-      <input type="text" name="category" class="box" placeholder="enter category name" required>
-      <input type="number" min="0" name="price" class="box" placeholder="enter product price" required>
-      <input type="file" name="image" accept="image/jpg, image/jpeg, image/png" class="box" required>
-      <input type="submit" value="add product" name="add_product" class="btn">
-   </form>
+<form action="" method="post" enctype="multipart/form-data">
+   <h3>add product</h3>
+   <input type="text" name="name" class="box" placeholder="enter product name" required>
+   <input type="text" name="category" class="box" placeholder="enter category name" required>
+   <input type="number" min="0" name="price" class="box" placeholder="enter product price" required>
+   <input type="file" name="image" accept="image/jpg, image/jpeg, image/png" class="box" required>
+   <input type="submit" value="add product" name="add_product" class="btn"> 
+   
+</form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
 
 </section>
 
@@ -120,14 +158,52 @@ if(isset($_POST['update_product'])){
 <!-- show products  -->
 
 <section class="show-products">
+      
+<?php
+         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+      
+<div class="product-display">
+   <table class="product-display-table">
+      <thead>
+      <tr>
+         <th>product image</th>
+         <th>product name</th>
+         <th>product category</th>
+         <th>product price</th>
+         <th>action</th>
+      </tr>
+      </thead>
+      <tr>
+         <td><img src="uploaded_img/<?php echo $fetch_products['image']; ?>" height="100" alt=""></td>
+         <td><?php echo $fetch_products['name']; ?></td>
+         <td><?php echo $fetch_products['category']; ?></td>
+         <td>₱<?php echo $fetch_products['price']; ?></td>
+         <td>
+         <a href="admin_products.php?update=<?php echo $fetch_products['id']; ?>" class="option-btn">update</a>
+            <a href="admin_products.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-btn" onclick="return confirm('delete this product?');">delete</a>
+         </td>
+      </tr>
+   </table>
+</div>
+<?php
+         }
+      }else{
+         echo '<p class="empty">no products added yet!</p>';
+      }
+      ?>
 
-   <div class="box-container">
 
+
+   <!--
       <?php
          $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
          if(mysqli_num_rows($select_products) > 0){
             while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
+
       <div class="box">
          <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
          <div class="name"><?php echo $fetch_products['name']; ?></div>
@@ -143,6 +219,7 @@ if(isset($_POST['update_product'])){
       }
       ?>
    </div>
+   -->
 
 </section>
 
@@ -160,6 +237,7 @@ if(isset($_POST['update_product'])){
       <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['image']; ?>">
       <img src="uploaded_img/<?php echo $fetch_update['image']; ?>" alt="">
       <input type="text" name="update_name" value="<?php echo $fetch_update['name']; ?>" class="box" required placeholder="enter product name">
+      <input type="text" name="update_cat" value="<?php echo $fetch_update['category']; ?>" class="box" required placeholder="enter product category">
       <input type="number" name="update_price" value="<?php echo $fetch_update['price']; ?>" min="0" class="box" required placeholder="enter product price">
       <input type="file" class="box" name="update_image" accept="image/jpg, image/jpeg, image/png">
       <input type="submit" value="update" name="update_product" class="btn">
@@ -175,9 +253,16 @@ if(isset($_POST['update_product'])){
 
 </section>
 
+
+
+
+
+
+
 <!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 <?php include 'alers.php' ?>
+
 </body>
 </html>
